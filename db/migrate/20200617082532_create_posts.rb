641a8ca0281a1d9ea8ccce0508cc6c4f183c4aa1
TARACHIN <yo.hi.0308@gmail.com>
2020-06-30 01:34:55 +0900

    First Push

---
+++ db/migrate/20200617082532_create_posts.rb
@@ -0,0 +1,12 @@
+class CreatePosts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :posts do |t|
+      t.string :title
+      t.string :detail
+      t.string :belong
+      t.string :name
+
+      t.timestamps
+    end
+  end
+end
